cc012d6ecdc2174c5f45df5c2a89802c346a3f85
Maksim Barkalov <maks@fizmatspb.ru>
2019-05-20 08:25:14 +0300

    Create neuro_func_test4.php

---
+++ neuro_func_test4.php
@@ -0,0 +1,378 @@
+<?php
+$allinput = [
+    [-10, 0.000045398899216871],
+    [-9.9, 0.00005017342334900],
+    [-9.8, 0.00005545006204910],
+    [-9.7, 0.00006128161729663],
+    [-9.6, 0.00006772644300342],
+    [-9.5, 0.00007484902862921],
+    [-9.4, 0.00008272064410971],
+    [-9.3, 0.00009142005253788],
+    [-9.2, 0.00010103429770048],
+    [-9.1, 0.00011165957432779],
+    [-9, 0.00012340218972334],
+    [-8.9, 0.00013637962635802],
+    [-8.8, 0.00015072171600687],
+    [-8.7, 0.00016657193711217],
+    [-8.6, 0.00018408884827573],
+    [-8.5, 0.00020344767212944],
+    [-8.4, 0.0002248420453117],
+    [-8.3, 0.00024848595191658],
+    [-8.2, 0.0002746158595852],
+    [-8.1, 0.00030349307939479],
+    [-8, 0.00033540637289566],
+    [-7.9, 0.00037067483205439],
+    [-7.8, 0.00040965106052538],
+    [-7.7, 0.00045272468759857],
+    [-7.6, 0.00050032624938591],
+    [-7.5, 0.00055293147536075],
+    [-7.4, 0.00061106602225307],
+    [-7.3, 0.00067531070158464],
+    [-7.2, 0.0007463072518276],
+    [-7.1, 0.00082476471132623],
+    [-7, 0.0009114664537742],
+    [-6.9, 0.0010072779542348],
+    [-6.8, 0.0011131553604646],
+    [-6.7, 0.0012301549517137],
+    [-6.6, 0.00135944357526],
+    [-6.5, 0.0015023101597543],
+    [-6.4, 0.0016601784140456],
+    [-6.3, 0.0018346208305893],
+    [-6.2, 0.0020273741238382],
+    [-6.1, 0.0022403562462492],
+    [-6, 0.0024756851377304],
+    [-5.9, 0.0027356993785361],
+    [-5.8, 0.0030229809308316],
+    [-5.7, 0.0033403801703674],
+    [-5.6, 0.0036910434269464],
+    [-5.5, 0.0040784432705707],
+    [-5.4, 0.0045064117992494],
+    [-5.3, 0.0049791772043271],
+    [-5.2, 0.0055014039096573],
+    [-5.1, 0.0060782366017791],
+    [-5, 0.006715348489118],
+    [-4.9, 0.0074189941486866],
+    [-4.8, 0.0081960673382675],
+    [-4.7, 0.0090541641698873],
+    [-4.6, 0.010001652055652],
+    [-4.5, 0.011047744848594],
+    [-4.4, 0.012202584607696],
+    [-4.3, 0.013477330416026],
+    [-4.2, 0.014884254671918],
+    [-4.1, 0.016436847252909],
+    [-4, 0.018149927917809],
+    [-3.9, 0.020039767260397],
+    [-3.8, 0.022124216454879],
+    [-3.7, 0.024422845933779],
+    [-3.6, 0.026957093008207],
+    [-3.5, 0.02975041827262],
+    [-3.4, 0.032828470424865],
+    [-3.3, 0.036219258870659],
+    [-3.2, 0.039953333162429],
+    [-3.1, 0.044063967938573],
+    [-3, 0.048587351573741],
+    [-2.9, 0.053562776217962],
+    [-2.8, 0.05903282628797],
+    [-2.7, 0.065043561776589],
+    [-2.6, 0.071644691967668],
+    [-2.5, 0.078889734292548],
+    [-2.4, 0.086836152153948],
+    [-2.3, 0.095545464597961],
+    [-2.2, 0.10508331976869],
+    [-2.1, 0.11551952317975],
+    [-2, 0.12692801104297],
+    [-1.9, 0.13938675828296],
+    [-1.8, 0.15297761052607],
+    [-1.7, 0.16778602938626],
+    [-1.6, 0.18390074088834],
+    [-1.5, 0.20141327798275],
+    [-1.4, 0.22041740991845],
+    [-1.3, 0.24100845383299],
+    [-1.2, 0.26328246733803],
+    [-1.1, 0.28733532511543],
+    [-1, 0.31326168751822],
+    [-0.90000000000002, 0.34115387473208],
+    [-0.80000000000002, 0.37110066594777],
+    [-0.70000000000002, 0.40318604888545],
+    [-0.60000000000002, 0.43748795048588],
+    [-0.50000000000002, 0.4740769841801],
+    [-0.40000000000002, 0.51301525239995],
+    [-0.30000000000002, 0.55435524446852],
+    [-0.20000000000002, 0.59813886938158],
+    [-0.10000000000002, 0.64439666007356],
+    [0, 0.69314718055994],
+    [0.099999999999981, 0.74439666007356],
+    [0.19999999999998, 0.79813886938158],
+    [0.29999999999998, 0.85435524446852],
+    [0.39999999999998, 0.91301525239994],
+    [0.49999999999998, 0.97407698418009],
+    [0.59999999999998, 1.0374879504859],
+    [0.69999999999998, 1.1031860488854],
+    [0.79999999999998, 1.1711006659478],
+    [0.89999999999998, 1.2411538747321],
+    [0.99999999999998, 1.3132616875182],
+    [1.1, 1.3873353251154],
+    [1.2, 1.463282467338],
+    [1.3, 1.541008453833],
+    [1.4, 1.6204174099184],
+    [1.5, 1.7014132779827],
+    [1.6, 1.7839007408883],
+    [1.7, 1.8677860293863],
+    [1.8, 1.9529776105261],
+    [1.9, 2.0393867582829],
+    [2, 2.126928011043],
+    [2.1, 2.2155195231797],
+    [2.2, 2.3050833197687],
+    [2.3, 2.3955454645979],
+    [2.4, 2.4868361521539],
+    [2.5, 2.5788897342925],
+    [2.6, 2.6716446919677],
+    [2.7, 2.7650435617766],
+    [2.8, 2.859032826288],
+    [2.9, 2.9535627762179],
+    [3, 3.0485873515737],
+    [3.1, 3.1440639679386],
+    [3.2, 3.2399533331624],
+    [3.3, 3.3362192588706],
+    [3.4, 3.4328284704248],
+    [3.5, 3.5297504182726],
+    [3.6, 3.6269570930082],
+    [3.7, 3.7244228459338],
+    [3.8, 3.8221242164549],
+    [3.9, 3.9200397672604],
+    [4, 4.0181499279178],
+    [4.1, 4.1164368472529],
+    [4.2, 4.2148842546719],
+    [4.3, 4.313477330416],
+    [4.4, 4.4122025846077],
+    [4.5, 4.5110477448486],
+    [4.6, 4.6100016520556],
+    [4.7, 4.7090541641699],
+    [4.8, 4.8081960673382],
+    [4.9, 4.9074189941487],
+    [5, 5.0067153484891],
+    [5.1, 5.1060782366018],
+    [5.2, 5.2055014039096],
+    [5.3, 5.3049791772043],
+    [5.4, 5.4045064117992],
+    [5.5, 5.5040784432705],
+    [5.6, 5.6036910434269],
+    [5.7, 5.7033403801703],
+    [5.8, 5.8030229809308],
+    [5.9, 5.9027356993785],
+    [6, 6.0024756851377],
+    [6.1, 6.1022403562462],
+    [6.2, 6.2020273741238],
+    [6.3, 6.3018346208306],
+    [6.4, 6.401660178414],
+    [6.5, 6.5015023101597],
+    [6.6, 6.6013594435752],
+    [6.7, 6.7012301549517],
+    [6.8, 6.8011131553604],
+    [6.9, 6.9010072779542],
+    [7, 7.0009114664537],
+    [7.1, 7.1008247647113],
+    [7.2, 7.2007463072518],
+    [7.3, 7.3006753107016],
+    [7.4, 7.4006110660222],
+    [7.5, 7.5005529314753],
+    [7.6, 7.6005003262494],
+    [7.7, 7.7004527246876],
+    [7.8, 7.8004096510605],
+    [7.9, 7.900370674832],
+    [8, 8.0003354063729],
+    [8.1, 8.1003034930794],
+    [8.2, 8.2002746158596],
+    [8.3, 8.3002484859519],
+    [8.4, 8.4002248420453],
+    [8.5, 8.5002034476721],
+    [8.6, 8.6001840888482],
+    [8.7, 8.7001665719371],
+    [8.8, 8.800150721716],
+    [8.9, 8.9001363796263],
+    [9, 9.0001234021897],
+    [9.1, 9.1001116595743],
+    [9.2, 9.2001010342977],
+    [9.3, 9.3000914200525],
+    [9.4, 9.4000827206441],
+    [9.5, 9.5000748490286],
+    [9.6, 9.600067726443],
+    [9.7, 9.7000612816173],
+    [9.8, 9.800055450062],
+    [9.9, 9.9000501734233],
+    [10, 10.000045398899]
+];
+
+$input = 4;
+
+$learning_rate = 0.00000009;
+
+function sigmoid(float $val)
+{
+    return (1 / (1 + exp(-$val)));
+}
+
+function dxsigmoid(float $val)
+{
+    return $val * (1 - $val);
+}
+
+$hidden_layer1 = array(
+    "brain" => [
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1)],
+    ],
+    "data" => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+);
+
+$hidden_layer2 = array(
+    "brain" => [
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+        [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+    ],
+    "data" => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+);
+
+$output_layer = array(
+    "brain" => [stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1), stats_rand_gen_normal(0, 1)],
+    "data" => 0
+);
+
+function flushData()
+{
+    global $output_layer;
+    global $hidden_layer1;
+    global $hidden_layer2;
+
+    for ($i = 0; $i < 10; ++$i) {
+        $hidden_layer1["data"][$i] = 0;
+        $hidden_layer2["data"][$i] = 0;
+    }
+    $output_layer["data"] = 0;
+
+}
+
+function calculate()
+{
+    flushData();
+    global $output_layer;
+    global $hidden_layer1;
+    global $hidden_layer2;
+    global $input;
+
+    for ($i = 0; $i < 10; ++$i) {
+        $hidden_layer1["data"][$i] = sigmoid($input * $hidden_layer1["brain"][$i][0]);
+    }
+
+    for ($i = 0; $i < 10; ++$i) {
+        for ($j = 0; $j < 10; ++$j) {
+            $hidden_layer2["data"][$i] += $hidden_layer1["data"][$j] * $hidden_layer2["brain"][$i][$j];
+        }
+        $hidden_layer2["data"][$i] = sigmoid($hidden_layer2["data"][$i]);
+    }
+
+    for ($i = 0; $i < 10; ++$i) {
+        $output_layer["data"] += $output_layer["brain"][$i] * $hidden_layer2["data"][$i];
+    }
+    //$output_layer["data"] = sigmoid($output_layer["data"]);
+
+}
+$cur_err = 0;
+function train()
+{
+    global $cur_err;
+    $cur_err = 0;
+    global $output_layer;
+    global $hidden_layer1;
+    global $hidden_layer2;
+    global $input;
+    global $allinput;
+    global $learning_rate;
+    //shuffle($allinput);
+    for ($k = 0; $k < count($allinput); ++$k) {
+        if ($k == 150) {
+            echo ")";
+        }
+        $input = $allinput[$k][0];
+        calculate();
+        //ошибка выходного слоя
+
+        $error = $output_layer["data"] - ($allinput[$k][1]/10);
+        $cur_err += $error*$error;
+        $pos = 1;
+        if ($error < 0) {
+            $pos = -1;
+        }
+        $error = $error * $error * $pos;
+        //$weights_delta = $error * dxsigmoid($error);
+        $weights_delta = $error;
+        for ($i = 0; $i < 10; ++$i) {
+            $output_layer["brain"][$i] = $output_layer["brain"][$i] - $hidden_layer2["data"][$i] * $weights_delta * $learning_rate;
+        }
+        //ошибка второго скрытого слоя
+        $arrayOfWeightsDelta = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for ($i = 0; $i < 10; ++$i) {
+            $error = $output_layer["brain"][$i] * $weights_delta;
+            $weights_delta = $error * dxsigmoid($error);
+            $arrayOfWeightsDelta[$i] = $weights_delta;
+            for ($j = 0; $j < 10; ++$j) {
+                $hidden_layer2["brain"][$i][$j] = $hidden_layer2["brain"][$i][$j] - $hidden_layer1["data"][$j] * $weights_delta * $learning_rate;
+            }
+        }
+        //ошибка перовго скрытого слоя
+        for ($i = 0; $i < 10; ++$i) {
+            $error = 0;
+            for ($j = 0; $j < 10; ++$j) {
+                $error += $hidden_layer2["brain"][$i][$j] * $arrayOfWeightsDelta[$i];
+            }
+            $weights_delta = $error * dxsigmoid($error);
+            $hidden_layer1["brain"][$i][0] = $hidden_layer1["brain"][$i][0] - $input * $weights_delta * $learning_rate;
+        }
+
+    }
+
+}
+
+function check_all()
+{
+    global $allinput;
+    global $input;
+    global $output_layer;
+    foreach ($allinput as $current) {
+        $input = $current[0];
+        calculate();
+        echo $input . " expected: " . $current[1] . " got: " . $output_layer["data"] . PHP_EOL;
+    }
+}
+
+echo "Before train: random weight" . PHP_EOL;
+check_all();
+
+
+for ($i = 0; $i < 60000; ++$i) {
+    train();
+    if ($i % 100 == 0) {
+        echo PHP_EOL . $i . PHP_EOL;
+        echo $output_layer["data"].PHP_EOL;
+        echo "Error: ".$cur_err;
+
+    }
+}
+echo PHP_EOL . "After train" . PHP_EOL;
+check_all();
